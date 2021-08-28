647adda07803d5ee62cc0212a6a4da71b8cbfd6a
you <erxmct3180@yahoo.ne.jp>
2021-08-28 11:30:54 +0900

    8/28

---
+++ git/js/script.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.o-button').click(function() { // ボタンがクリックされたら
+        $(this).toggleClass(); // ボタン自身にactiveクラスを付与し
+    })
+});
+
+// $(function() {
+// alert('OK!');
+// });
