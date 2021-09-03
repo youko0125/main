b7cad44a5017542ac7afdaaee07b9d1607efb76d
you <erxmct3180@yahoo.ne.jp>
2021-09-03 09:16:44 +0900

    9/3

--- git/js/script.js
+++ git/js/script.js
@@ -1,10 +1,10 @@
 $(function() {
     $('.o-button').click(function() { // ボタンがクリックされたら
         $('.l-side').toggleClass('active'); // ナビにクラスを付与
-        $('.o-dli-close').toggleClass('one'); // ×にクラスを付与
-    })
+        $('.o-dli-close').show();
+    });
+    $('.o-dli-close').click(function() {
+        $(this).toggleClass('open'); // ×にクラスを付与
+        $('.l-side').toggleClass('active')
+    });
 });
-
-// $(function() {
-// alert('OK!');
-// });
