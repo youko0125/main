a6101ac25ea0649e9b7c95789bfcd1c67b699be0
you <erxmct3180@yahoo.ne.jp>
2021-11-07 09:00:06 +0900

    11/07

--- git/js/script.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $('.o-button').click(function() { // ボタンがクリックされたら
-        $('.l-side').addClass('active'); // ナビにクラスを付与
-    });
-    $('.o-side_button').click(function() {
-        $('.l-side').removeClass('active')
-    });
-});
